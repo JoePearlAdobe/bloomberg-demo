fbd3aa4b51dab959b12fa57268af36f1a35c4f21
Darin Kuntze <dkuntze@gmail.com>
2025-04-21 15:26:45 -0500

    wip - day one

---
+++ blocks/video-hero/video-hero.js
@@ -0,0 +1,20 @@
+import { readBlockConfig } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+  const cfg = readBlockConfig(block);
+  const videoHeroContainer = document.createElement('div');
+  const videoHero = document.createElement('video');
+  videoHero.setAttribute('autoplay', '');
+  videoHero.setAttribute('loop', '');
+  videoHero.setAttribute('muted', '');
+  videoHero.setAttribute('playsinline', '');
+  videoHero.classList.add('video-hero');
+  const source = document.createElement('source');
+  source.setAttribute('src', cfg.video);
+  videoHero.appendChild(source);
+  videoHeroContainer.append(videoHero);
+  videoHeroContainer.className = 'video-hero-container';
+
+  block.replaceWith(videoHeroContainer);
+  console.log(cfg);
+}
