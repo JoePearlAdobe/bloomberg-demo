90329d39980708a5f8cdddbf8edafe63890b2fe1
Darin Kuntze <dkuntze@gmail.com>
2025-04-23 10:55:44 -0500

    jump links mobile fix

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,11 @@
 // add delayed functionality here
+
+/* jumplinks mobile menu */
+const jumplinks = document.querySelector('body.product-landing div.section.jumplinks');
+const jumplinksUL = document.querySelector('body.product-landing div.section.jumplinks ul');
+const firstItem = jumplinks.querySelector('li:first-of-type');
+
+firstItem.addEventListener('click', () => {
+    jumplinksUL.classList.toggle('expanded');
+    jumplinks.classList.toggle('expanded');
+});
