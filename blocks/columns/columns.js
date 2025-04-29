783f46d6c1f17a89c44183c10763af8cb0c825f5
joe-pearl-adobe <jpearl@adobe.com>
2025-04-29 17:06:37 -0400

    put columns code and css back to what was in main

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,28 +1,6 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
-  
-  // Check for black background style variant
-  const parentSection = block.closest('.section');
-  if (parentSection && parentSection.classList.contains('black-background')) {
-    block.classList.add('black-background');
-    document.body.classList.add('black');
-  }
-
-  // Handle black background and theme markers
-  const blackBackgroundMarker = block.querySelector('.black-background-marker');
-  const blackThemeMarker = block.querySelector('.black-marker');
-
-  if (blackBackgroundMarker) {
-    block.classList.add('black-background');
-    document.body.classList.add('black');
-    blackBackgroundMarker.remove();
-  }
-
-  if (blackThemeMarker) {
-    document.body.classList.add('black');
-    blackThemeMarker.remove();
-  }
 
   // setup image columns
   [...block.children].forEach((row) => {
