a9b248592c4e19dd39d6a05f54a09b5bd52264da
joe-pearl-adobe <jpearl@adobe.com>
2025-04-29 16:25:29 -0400

    added a black class to the columns blocks

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,6 +2,20 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  // Check for black background style variant
+  const parentSection = block.closest('.section');
+  if (parentSection && parentSection.classList.contains('black-background')) {
+    block.classList.add('black-background');
+  }
+
+  // Alternative: check for a specific marker in the block content
+  if (block.querySelector('.black-background-marker')) {
+    block.classList.add('black-background');
+    // Remove the marker as it's only used for signaling
+    const marker = block.querySelector('.black-background-marker');
+    if (marker) marker.remove();
+  }
+
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
