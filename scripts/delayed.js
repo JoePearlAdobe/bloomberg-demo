4a9a2f2382d2e2b052560175c297602336da97d9
Darin Kuntze <dkuntze@gmail.com>
2025-04-30 09:58:07 -0500

    aadd null checks

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -3,9 +3,11 @@
 /* jumplinks mobile menu */
 const jumplinks = document.querySelector('body.product-landing div.section.jumplinks');
 const jumplinksUL = document.querySelector('body.product-landing div.section.jumplinks ul');
-const firstItem = jumplinks.querySelector('li:first-of-type');
+const firstItem = jumplinks?.querySelector('li:first-of-type');
 
-firstItem.addEventListener('click', () => {
-  jumplinksUL.classList.toggle('expanded');
-  jumplinks.classList.toggle('expanded');
-});
+if (firstItem && jumplinksUL && jumplinks) {
+  firstItem.addEventListener('click', () => {
+    jumplinksUL.classList.toggle('expanded');
+    jumplinks.classList.toggle('expanded');
+  });
+}
