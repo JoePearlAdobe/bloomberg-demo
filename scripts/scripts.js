68a69924c16ab3928c1fc5806bdd00298f52ccb6
Darin Kuntze <dkuntze@gmail.com>
2025-05-07 16:46:02 -0500

    fix more vh issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -281,11 +281,6 @@ const branch = searchParams.get('nx') || 'main';
 
 export const NX_ORIGIN = 'https://da.live/nx';
 
-if (searchParams.get('dapreview')) {
-  // vh hack to work with dapreview
-  document.body.classList.add('dapreview');
-}
-
 (async function loadDa() {
   /* eslint-disable import/no-unresolved */
   if (searchParams.get('dapreview')) {
